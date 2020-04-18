c443df123b8adf610d3c2d1fa94908205852c014
chadwyck242 <chadwyck.codeart@gmail.com>
2020-04-17 21:15:31 -0600

    Add part1 exercise 1.13 anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -10,18 +10,34 @@ const anecdotes = [
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
 
+const zeroArray = new Array(6).fill(0);
+let votesArray = [...zeroArray];
+
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(0);
 
-  const getRandomNumber = () => {
+  const getRandomSelection = () => {
     const randomNumber = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomNumber);
+    setVotes(votesArray[randomNumber]);
+  };
+
+  const setSelectedVotes = () => {
+    console.log(votesArray);
+    let voteIndex = anecdotes.indexOf(anecdotes[selected]);
+    console.log(voteIndex);
+    votesArray[voteIndex] += 1;
+    console.log(votesArray);
+    setVotes(votesArray[voteIndex]);
   };
 
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
-      <button onClick={() => getRandomNumber()}>next anecdote</button>
+      <p>has {votes} votes</p>
+      <button onClick={() => setSelectedVotes()}>vote</button>
+      <button onClick={() => getRandomSelection()}>next anecdote</button>
     </div>
   );
 };
