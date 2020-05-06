71d071ffd46c048edce2cfc8e2e29b208d4c7010
chadwyck242 <chadwyck.codeart@gmail.com>
2020-05-05 21:39:54 -0600

    Add completed part2 exercise 2.5: separate module

---
+++ part2/coursecontents/src/components/Course.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((allParts, part) => {
+    return allParts + part.exercises;
+  }, 0);
+  return (
+    <strong>
+      <p>Total of {sum} exercises</p>
+    </strong>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      {courses.map((course) => (
+        <>
+          <Header key={course.id} name={course.name} />
+          <Content key={course.id} parts={course.parts} />
+          <Total key={course.id} parts={course.parts} />
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -1,53 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Total = ({ parts }) => {
-  // const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises;
-  const sum = parts.reduce((allParts, part) => {
-    return allParts + part.exercises;
-  }, 0);
-  return (
-    <strong>
-      <p>Total of {sum} exercises</p>
-    </strong>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map((course) => (
-        <>
-          <Header key={course.id} name={course.name} />
-          <Content key={course.id} parts={course.parts} />
-          <Total key={course.id} parts={course.parts} />
-        </>
-      ))}
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
